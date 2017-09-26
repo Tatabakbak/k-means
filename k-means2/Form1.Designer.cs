d2f4de27add3b79594c9bae08110f81d4da3a12a
Тян-Тян <->
2017-09-26 16:33:49 +0300

    Add project files.

---
+++ k-means2/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace k_means2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_random = new System.Windows.Forms.RadioButton();
+            this.rb_group = new System.Windows.Forms.RadioButton();
+            this.clusters_group = new System.Windows.Forms.GroupBox();
+            this.rb_4 = new System.Windows.Forms.RadioButton();
+            this.rb_3 = new System.Windows.Forms.RadioButton();
+            this.rb_2 = new System.Windows.Forms.RadioButton();
+            this.rb_1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.clusters_group.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(322, 404);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(403, 404);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(886, 9);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(67, 13);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "CLUSTERS:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb_random);
+            this.groupBox1.Controls.Add(this.rb_group);
+            this.groupBox1.Location = new System.Drawing.Point(786, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(78, 70);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dataset";
+            // 
+            // rb_random
+            // 
+            this.rb_random.AutoSize = true;
+            this.rb_random.Location = new System.Drawing.Point(5, 42);
+            this.rb_random.Name = "rb_random";
+            this.rb_random.Size = new System.Drawing.Size(65, 17);
+            this.rb_random.TabIndex = 1;
+            this.rb_random.Text = "Random";
+            this.rb_random.UseVisualStyleBackColor = true;
+            this.rb_random.CheckedChanged += new System.EventHandler(this.rb_random_CheckedChanged);
+            // 
+            // rb_group
+            // 
+            this.rb_group.AutoSize = true;
+            this.rb_group.Checked = true;
+            this.rb_group.Location = new System.Drawing.Point(5, 19);
+            this.rb_group.Name = "rb_group";
+            this.rb_group.Size = new System.Drawing.Size(59, 17);
+            this.rb_group.TabIndex = 0;
+            this.rb_group.TabStop = true;
+            this.rb_group.Text = "Groups";
+            this.rb_group.UseVisualStyleBackColor = true;
+            this.rb_group.CheckedChanged += new System.EventHandler(this.rb_group_CheckedChanged);
+            // 
+            // clusters_group
+            // 
+            this.clusters_group.Controls.Add(this.rb_4);
+            this.clusters_group.Controls.Add(this.rb_3);
+            this.clusters_group.Controls.Add(this.rb_2);
+            this.clusters_group.Controls.Add(this.rb_1);
+            this.clusters_group.Location = new System.Drawing.Point(786, 85);
+            this.clusters_group.Name = "clusters_group";
+            this.clusters_group.Size = new System.Drawing.Size(78, 120);
+            this.clusters_group.TabIndex = 5;
+            this.clusters_group.TabStop = false;
+            this.clusters_group.Text = "Clusters";
+            // 
+            // rb_4
+            // 
+            this.rb_4.AutoSize = true;
+            this.rb_4.Location = new System.Drawing.Point(5, 91);
+            this.rb_4.Name = "rb_4";
+            this.rb_4.Size = new System.Drawing.Size(31, 17);
+            this.rb_4.TabIndex = 6;
+            this.rb_4.TabStop = true;
+            this.rb_4.Text = "4";
+            this.rb_4.UseVisualStyleBackColor = true;
+            this.rb_4.CheckedChanged += new System.EventHandler(this.rb_4_CheckedChanged);
+            // 
+            // rb_3
+            // 
+            this.rb_3.AutoSize = true;
+            this.rb_3.Checked = true;
+            this.rb_3.Location = new System.Drawing.Point(5, 68);
+            this.rb_3.Name = "rb_3";
+            this.rb_3.Size = new System.Drawing.Size(31, 17);
+            this.rb_3.TabIndex = 2;
+            this.rb_3.TabStop = true;
+            this.rb_3.Text = "3";
+            this.rb_3.UseVisualStyleBackColor = true;
+            this.rb_3.CheckedChanged += new System.EventHandler(this.rb_3_CheckedChanged);
+            // 
+            // rb_2
+            // 
+            this.rb_2.AutoSize = true;
+            this.rb_2.Location = new System.Drawing.Point(5, 43);
+            this.rb_2.Name = "rb_2";
+            this.rb_2.Size = new System.Drawing.Size(31, 17);
+            this.rb_2.TabIndex = 1;
+            this.rb_2.Text = "2";
+            this.rb_2.UseVisualStyleBackColor = true;
+            this.rb_2.CheckedChanged += new System.EventHandler(this.rb_2_CheckedChanged);
+            // 
+            // rb_1
+            // 
+            this.rb_1.AutoSize = true;
+            this.rb_1.Location = new System.Drawing.Point(5, 20);
+            this.rb_1.Name = "rb_1";
+            this.rb_1.Size = new System.Drawing.Size(31, 17);
+            this.rb_1.TabIndex = 0;
+            this.rb_1.Text = "1";
+            this.rb_1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.rb_1.UseVisualStyleBackColor = true;
+            this.rb_1.CheckedChanged += new System.EventHandler(this.rb_1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 439);
+            this.Controls.Add(this.clusters_group);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnClear);
+            this.Name = "Form1";
+            this.Text = "K-means clustering";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.clusters_group.ResumeLayout(false);
+            this.clusters_group.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.Button btnGo;
+		private System.Windows.Forms.Label lbl1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton rb_random;
+		private System.Windows.Forms.RadioButton rb_group;
+        private System.Windows.Forms.GroupBox clusters_group;
+        private System.Windows.Forms.RadioButton rb_3;
+        private System.Windows.Forms.RadioButton rb_2;
+        private System.Windows.Forms.RadioButton rb_1;
+        private System.Windows.Forms.RadioButton rb_4;
+    }
+}
+
---
+++ k-means2/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace k_means2
+{
+	public partial class Form1 : Form
+	{
+
+		double[][] rawData;
+		double[][] means;
+		int numClusters;
+        int checkedNumClusters;
+        int[] clustering;
+		bool clustered = false;
+		bool meansComputed = false;
+        
+		Brush[] brushes = { Brushes.LimeGreen, Brushes.Blue, Brushes.Orange, Brushes.Red, Brushes.Violet, Brushes.Turquoise };
+
+
+		public Form1()
+		{
+			InitializeComponent();
+			InitializeTuples();
+            btnClear.Enabled = false;
+            
+            clusters_group.Enabled = false;
+            checkedNumClusters = 3;
+
+        }
+
+		// задаем набор случайных даных
+		private void InitializeTuples()
+		{
+			//numClusters = Int32.Parse(txt_num_clusters.Text);
+
+			if (rb_group.Checked)
+			{
+				rawData = new double[20][];
+				rawData[0] = new double[] { 422.0, 225.0 };
+				rawData[1] = new double[] { 223.0, 160.0 };
+				rawData[2] = new double[] { 59.0, 110.0 };
+				rawData[3] = new double[] { 66.0, 120.0 };
+				rawData[4] = new double[] { 235.0, 150.0 };
+				rawData[5] = new double[] { 467.0, 240.0 };
+				rawData[6] = new double[] { 468.0, 200.0 };
+				rawData[7] = new double[] { 470.0, 250.0 };
+				rawData[8] = new double[] { 45.0, 130.0 };
+				rawData[9] = new double[] { 466.0, 210.0 };
+				rawData[10] = new double[] { 217.0, 190.0 };
+				rawData[11] = new double[] { 245.0, 180.0 };
+				rawData[12] = new double[] { 234.0, 170.0 };
+				rawData[13] = new double[] { 450.0, 214.0 };
+				rawData[14] = new double[] { 61.0, 110.0 };
+				rawData[15] = new double[] { 50.0, 100.0 };
+				rawData[16] = new double[] { 466.0, 225.0 };
+				rawData[17] = new double[] { 67.0, 120.0 };
+				rawData[18] = new double[] { 498.0, 205.0 };
+				rawData[19] = new double[] { 75.0, 130.0 };
+			}
+			else
+			{
+				rawData = new double[300][];
+				Random random = new Random();
+				for (int i = 0; i < rawData.Length; i++)
+				{
+					rawData[i] = new double[] { random.Next(0, 750), random.Next(0, 350) };
+				}
+			}
+
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			Graphics g = e.Graphics;
+			g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+			Brush b = new SolidBrush(Color.Black);
+			if (!clustered)
+			{		
+				foreach (var p in rawData)
+				{
+					Point pPosition = new Point((int)p[0], (int)p[1]);
+					g.FillEllipse(b, new RectangleF(pPosition, new Size(6, 6)));
+				}
+			}
+			else
+			{
+				for (int k = 0; k < numClusters; ++k)
+				{
+					for (int i = 0; i < rawData.Length; ++i)
+					{
+						int clusterID = clustering[i];
+						b = brushes[clusterID];
+						if (clusterID != k) continue;
+						Point pPosition = new Point((int)rawData[i][0], (int)rawData[i][1]);
+						g.FillEllipse(b, new RectangleF(pPosition, new Size(5, 5)));
+					}
+				}
+			}
+
+			if (meansComputed)
+			{
+				for (int i =0; i < numClusters; i++)
+				{
+					b = brushes[i];
+					Point pPosition = new Point((int)means[i][0], (int)means[i][1]);
+					g.FillEllipse(b, new RectangleF(pPosition, new Size(11, 11)));
+				}
+			}
+		}
+
+		// возращает массив кластеров для исходного набора данных
+		public int[] Cluster()
+		{
+            numClusters = checkedNumClusters;
+            means = new double[numClusters][]; //массив для центроидов
+            for (int k = 0; k < numClusters; ++k)
+                means[k] = new double[rawData[0].Length];
+            //double[][] data = rawData;
+
+            bool changed = true; // были изменения в принадлежности к кластеру
+			bool success = true; // все точки приписаны к кластерам
+
+			clustering = InitClustering(); // приписываем данным случайные кластеры
+			clustered = true;
+
+			while (changed == true && success == true)
+			{
+				Refresh();
+				System.Threading.Thread.Sleep(1500);
+				success = UpdateMeans(); // вычисление нового центроида
+				changed = UpdateClustering(); // вычисление ближайшего центроида для каждой точки
+			}
+			return clustering;
+		}
+
+		//приписываем каждую точку к случайному кластеру так, чтобы к каждому кластеру была приписана хотя бы одна точка
+		private int[] InitClustering()
+		{
+			
+			Random random = new Random();
+			int[] clustering = new int[rawData.Length];
+			for (int i = 0; i < numClusters; ++i) 
+				clustering[i] = i;
+			for (int i = numClusters; i < clustering.Length; ++i)
+				clustering[i] = random.Next(0, numClusters); 
+			return clustering; 
+		}
+		
+		// обновление значений центроидов
+		private bool UpdateMeans()
+		{
+			int[] clusterCounts = new int[numClusters]; //количество точек по кластерам
+			for (int i = 0; i < rawData.Length; ++i)
+			{
+				int cluster = clustering[i];
+				++clusterCounts[cluster];
+			}
+
+			for (int k = 0; k < numClusters; ++k)
+				if (clusterCounts[k] == 0)
+					return false; // проверка - к каждому кластеру должны быть приписаны точки
+			
+			for (int k = 0; k < means.Length; ++k) //обнуление центроидов
+				for (int j = 0; j < means[k].Length; ++j)
+					means[k][j] = 0.0;
+
+			for (int i = 0; i < rawData.Length; ++i)
+			{
+				int cluster = clustering[i];
+				for (int j = 0; j < rawData[i].Length; ++j)
+					means[cluster][j] += rawData[i][j]; // записывается сумма координат точек, приписанных кластеру
+			}
+
+			for (int k = 0; k < means.Length; ++k)
+				for (int j = 0; j < means[k].Length; ++j)
+					means[k][j] /= clusterCounts[k]; // находим среднее 
+			meansComputed = true;
+			return true;
+		}
+
+		// обновление распределений по кластерам
+		private bool UpdateClustering()
+		{
+            bool changed = false;
+
+			int[] newClustering = new int[clustering.Length]; // массив для нового результата
+			Array.Copy(clustering, newClustering, clustering.Length);
+
+			double[] distances = new double[numClusters]; // расстояние от точки до центроидов
+
+			for (int i = 0; i < rawData.Length; ++i) // рассчитываем расстоение до цетроидов для всех точек
+			{
+				for (int k = 0; k < numClusters; ++k)
+					distances[k] = Distance(rawData[i], means[k]); 
+
+				int newClusterID = MinIndex(distances); // находим ближайший центроид
+				if (newClusterID != newClustering[i])
+				{
+					changed = true;
+					newClustering[i] = newClusterID; 
+				}
+			}
+
+			if (changed == false) // если изменений в распределении точек по кластерам нет - выход
+				return false; 
+
+			int[] clusterCounts = new int[numClusters];
+			for (int i = 0; i < rawData.Length; ++i)
+			{
+				int cluster = newClustering[i];
+				++clusterCounts[cluster];
+			}
+
+			for (int k = 0; k < numClusters; ++k)
+				if (clusterCounts[k] == 0)
+					return false; // к каждому кластеру должны быть приписаны точки
+
+			Array.Copy(newClustering, clustering, newClustering.Length);  // копируем полученный массив в исходный
+			return true; // если внесены изменеия
+		}
+
+		//разница между векторами точки и центроида
+		private double Distance(double[] tuple, double[] mean)
+		{
+			double sumSquaredDiffs = 0.0;
+			for (int j = 0; j < tuple.Length; ++j)
+				sumSquaredDiffs += Math.Pow((tuple[j] - mean[j]), 2);
+			return Math.Sqrt(sumSquaredDiffs);
+		}
+
+		private int MinIndex(double[] distances)
+		{
+			int indexOfMin = 0;
+			double smallDist = distances[0];
+			for (int k = 0; k < distances.Length; ++k)
+			{
+				if (distances[k] < smallDist)
+				{
+					smallDist = distances[k];
+					indexOfMin = k;
+				}
+			}
+			return indexOfMin;
+		}
+
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			lbl1.Text = "CLUSTERS:";
+			clustered = false;
+			meansComputed = false;
+			InitializeTuples();
+			Refresh();
+            btnGo.Enabled = true;
+        }
+
+		private void btnGo_Click(object sender, EventArgs e)
+		{
+            btnClear.Enabled = false;
+            btnGo.Enabled = false;
+            clustering = Cluster();		
+			for (int i =0; i< rawData.Length; i++)
+			{
+				lbl1.Text +="\n"+ i + ". " + clustering[i];
+			}
+			clustered = true;
+			Refresh();
+            btnClear.Enabled = true;
+            
+        }
+
+        private void rb_random_CheckedChanged(object sender, EventArgs e)
+        {
+            clusters_group.Enabled = true;
+           
+            lbl1.Text = "CLUSTERS:";
+            clustered = false;
+            meansComputed = false;
+            InitializeTuples();
+            Refresh();
+        }
+
+        private void rb_group_CheckedChanged(object sender, EventArgs e)
+        {
+            rb_3.Checked = true; 
+            clusters_group.Enabled = false;
+        }
+
+
+        private void rb_1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkedNumClusters = 1;
+        }
+
+        private void rb_2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkedNumClusters = 2;
+        }
+
+        private void rb_3_CheckedChanged(object sender, EventArgs e)
+        {
+            checkedNumClusters = 3;
+        }
+
+        private void rb_4_CheckedChanged(object sender, EventArgs e)
+        {
+            checkedNumClusters = 4;
+        }
+    }
+}
